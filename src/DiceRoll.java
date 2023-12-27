33de34bb702cc435686ef1155cb17b7f43882e70
byakuran <nwo1318@gmail.com>
2023-12-27 17:43:37 +0800

    simulate dice ro

---
+++ src/DiceRoll.java
@@ -0,0 +1,10 @@
+public class DiceRoll {
+    public static void main(String[] args) throws Exception {
+        int random = (int)(Math.random()*6)+1;
+        int random_2 = (int)(Math.random()*6)+1;
+        System.out.println("The first die is: " + random);
+        System.out.println("The second die is: " + random_2);
+        System.out.print("Total roll: ");
+        System.out.print(random + random_2);
+    }
+}
